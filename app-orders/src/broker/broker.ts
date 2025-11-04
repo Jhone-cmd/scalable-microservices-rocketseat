b6bf28cb2e2f2e6bb871864bdf3df93d786c3abd
jhone-cmd <jhone.1941@gmail.com>
2025-11-04 10:15:04 -0300

    sending a message to the rabbit

--- app-orders/src/broker/broker.ts
+++ app-orders/src/broker/broker.ts
@@ -1,4 +1,4 @@
-import { env } from '@/env/schema.ts'
+import { env } from '../env/schema.ts'
 import amqp from 'amqplib'
 
 export const broker = await amqp.connect(env.BROKER_URL)
--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify"
 import fastifyCors from "@fastify/cors"
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
+import { channels } from "../broker/channels/index.ts"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -20,10 +21,14 @@ app.post('/orders', {
             amount: z.coerce.number()
         })
     }
-}, (request, reply) => {
+}, async (request, reply) => {
     const { amount } = request.body
 
     console.log('Creating an order with amount', amount);
+
+    channels.orders.sendToQueue('orders', Buffer.from(JSON.stringify({ amount })))
+
+    return reply.status(201).send()
 
 })
 
