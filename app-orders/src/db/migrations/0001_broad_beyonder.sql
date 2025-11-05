9ba8c559a6f3ea198d1d0fbf8c59372aaebaa61d
jhone-cmd <jhone.1941@gmail.com>
2025-11-05 09:24:49 -0300

    migrating the table data and making the connection.

---
+++ app-orders/src/db/migrations/0001_broad_beyonder.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "customers" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"address" text NOT NULL,
+	"state" text NOT NULL,
+	"zip_code" text NOT NULL,
+	"country" text NOT NULL,
+	"date_of_birth" date,
+	"create_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "customers_email_unique" UNIQUE("email")
+);
