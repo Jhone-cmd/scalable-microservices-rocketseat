39fe2d1cc562fbe84f57d28dc7c20802cb9375c2
jhone-cmd <jhone.1941@gmail.com>
2025-11-05 09:21:05 -0300

    create customers table

---
+++ app-orders/src/db/schema/customers.ts
@@ -0,0 +1,16 @@
+import { date, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+import { randomUUID } from "node:crypto";
+import { create } from "node:domain";
+
+export const customers = pgTable('customers', {
+    id: text().primaryKey().$defaultFn(() => randomUUID()),
+    name: text().notNull(),
+    email: text().unique().notNull(),
+    address: text().notNull(),
+    state: text().notNull(),
+    zipCode: text().notNull(),
+    country: text().notNull(),
+    dateOfBirth: date({ mode: 'date' }),
+    createAt: timestamp().defaultNow().notNull()
+})
