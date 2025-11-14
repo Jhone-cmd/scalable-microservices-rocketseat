cb2bde3d4aaedddb2cd8c6fb04be0211bcc8c1b1
jhone-cmd <jhone.1941@gmail.com>
2025-11-14 09:27:34 -0300

    placing/sending the docker image to the repository

--- infra/index.ts
+++ infra/index.ts
@@ -1,7 +1,18 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as docker from "@pulumi/docker-build"
 
-const orderECRRepository = new awsx.ecr.Repository('orders-ecr', {
+const ordersECRRepository = new awsx.ecr.Repository('orders-ecr', {
     forceDelete: true
+})
+
+export const ordersDockerImage = new docker.Image('orders-image', {
+    context: {
+        location: '../app-orders',
+    },
+    push: true,
+    registries: [{
+        address: ordersECRRepository.repository.repositoryUrl
+    }]
 })
