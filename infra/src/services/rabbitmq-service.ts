60ff5f057147b989e263b01dd45d92e27b91bd81
jhone-cmd <jhone.1941@gmail.com>
2025-11-18 09:21:44 -0300

    configuring the load balancer in the RabbitMQ service for visibility.

--- infra/src/services/rabbitmq-service.ts
+++ infra/src/services/rabbitmq-service.ts
@@ -1,5 +1,21 @@
 import * as awsx from "@pulumi/awsx";
 import { cluster } from "../cluster";
+import { appLoadBalancer, networkLoadBalancer } from "../load-balancer"
+
+export const rabbitMQAdminTargetGroup = appLoadBalancer.createTargetGroup('rabbitmq-admin-target', {
+    port: 15672,
+    protocol: 'HTTP',
+    healthCheck: {
+        path: '/',
+        protocol: 'HTTP'
+    }
+})
+
+export const rabbitMQAdminHttpListener = appLoadBalancer.createListener('rabbitmq-admin-listener', {
+    port: 15672,
+    protocol: 'HTTP',
+    targetGroup: rabbitMQAdminTargetGroup
+})
 
 export const rabbitMQService = new awsx.classic.ecs.FargateService('fargate-rabbitmq', {
     cluster,
@@ -10,6 +26,9 @@ export const rabbitMQService = new awsx.classic.ecs.FargateService('fargate-rabb
             image: 'rabbitmq:3-management',
             cpu: 256,
             memory: 512,
+            portMappings: [
+                rabbitMQAdminHttpListener
+            ],
             environment: [
                 { name: 'RABBIT_DEFAULT_USER', value: 'admin' },
                 { name: 'RABBIT_DEFAULT_PASS', value: 'admin' }
