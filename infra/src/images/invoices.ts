01805f3bc79ebceb3e1efbc5fb5c19591ace62b2
jhone-cmd <jhone.1941@gmail.com>
2025-11-20 09:27:50 -0300

    create a invoices image with pulumi

---
+++ infra/src/images/invoices.ts
@@ -0,0 +1,24 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+import * as docker from "@pulumi/docker-build"
+
+const invoicesECRRepository = new awsx.ecr.Repository('invoices-erc', {
+    forceDelete: true
+})
+
+export const ordersDockerImage = new docker.Image('invoices-image', {
+    tags: [
+        pulumi.interpolate`${invoicesECRRepository.repository.repositoryUrl}:latest`
+    ],
+    context: {
+        location: '../app-invoices',
+    },
+    platforms: [
+        'linux/amd64'
+    ],
+    push: true,
+    registries: [{
+        address: invoicesECRRepository.repository.repositoryUrl,
+    }]
+})
