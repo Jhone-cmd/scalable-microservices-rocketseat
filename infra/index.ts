8d210ba5218c4d4276b3a0c39c15780d5fe23c06
jhone-cmd <jhone.1941@gmail.com>
2025-11-21 15:23:00 -0300

    add kong service in index.ts file

--- infra/index.ts
+++ infra/index.ts
@@ -2,11 +2,13 @@ import { appLoadBalancer } from './src/load-balancer'
 import { ordersService } from './src/services/orders-service'
 import { invoicesService } from './src/services/invoices-service'
 import { rabbitMQService } from './src/services/rabbitmq-service'
+import { kongService } from './src/services/kong-service'
 import * as pulumi from '@pulumi/pulumi'
 
 export const ordersId = ordersService.service.id
 export const invoicesId = invoicesService.service.id
 export const rabbitMQId = rabbitMQService.service.id
+export const kongId = kongService.service.id
 export const rabbitMQAdminUrl = pulumi.interpolate`http://${appLoadBalancer.listeners[0].endpoint.hostname}:15672`
 
 
