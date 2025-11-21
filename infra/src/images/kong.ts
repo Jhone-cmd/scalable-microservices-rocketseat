f39d0936a86ec70bd970180e8f5cad78beeaf88c
jhone-cmd <jhone.1941@gmail.com>
2025-11-21 15:06:51 -0300

    create a kong image with pulumi

---
+++ infra/src/images/kong.ts
@@ -0,0 +1,30 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+import * as docker from "@pulumi/docker-build"
+
+const kongECRRepository = new awsx.ecr.Repository('kong-erc', {
+    forceDelete: true
+})
+
+const kongECRToken = aws.ecr.getAuthorizationTokenOutput({
+    registryId: kongECRRepository.repository.registryId
+})
+
+export const kongDockerImage = new docker.Image('kong-image', {
+    tags: [
+        pulumi.interpolate`${kongECRRepository.repository.repositoryUrl}:latest`
+    ],
+    context: {
+        location: '../docker/kong',
+    },
+    platforms: [
+        'linux/amd64'
+    ],
+    push: true,
+    registries: [{
+        address: kongECRRepository.repository.repositoryUrl,
+        username: kongECRToken.userName,
+        password: kongECRToken.password
+    }]
+})
